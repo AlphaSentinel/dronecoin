b49d2633f58fd9e195057599fc67d69aaaa13eea
adriangrassi <adriangrassi@gmail.com>
2017-12-30 00:02:33 +0000

    Initial DroneCoin Commit

---
+++ build/release/tests/CMakeFiles/UnitTests.dir/DependInfo.cmake
@@ -0,0 +1,93 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ubuntu/dronecoin/tests/UnitTests/ArrayRefTests.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ArrayRefTests.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/ArrayViewTests.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ArrayViewTests.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/Base58.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Base58.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/BlockReward.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/BlockReward.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/BlockingQueue.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/BlockingQueue.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/Chacha8.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Chacha8.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/Checkpoints.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Checkpoints.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/DecomposeAmountIntoDigits.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/DecomposeAmountIntoDigits.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/EventWaiter.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/EventWaiter.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/ICoreStub.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ICoreStub.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/ICryptoNoteProtocolQueryStub.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ICryptoNoteProtocolQueryStub.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/INodeStubs.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/INodeStubs.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/MulDiv.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/MulDiv.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/ParseAmount.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/ParseAmount.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/PaymentGateTests.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/PaymentGateTests.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/Serialization.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Serialization.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/SerializationKV.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/SerializationKV.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/Shuffle.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/Shuffle.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/StringBufferTests.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/StringBufferTests.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/StringViewTests.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/StringViewTests.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestBcS.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestBcS.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestBlockchainExplorer.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestBlockchainExplorer.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestBlockchainGenerator.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestBlockchainGenerator.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestCurrency.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestCurrency.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestFormatUtils.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestFormatUtils.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestInprocessNode.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestInprocessNode.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestJsonValue.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestJsonValue.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestMessageQueue.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestMessageQueue.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestPath.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestPath.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestPeerlist.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestPeerlist.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestProtocolPack.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestProtocolPack.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestTransactionPoolDetach.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransactionPoolDetach.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestTransfers.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfers.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestTransfersConsumer.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersConsumer.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestTransfersContainer.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersContainer.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestTransfersContainerKeyImage.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersContainerKeyImage.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestTransfersSubscription.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestTransfersSubscription.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestWallet.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestWallet.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestWalletLegacy.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestWalletLegacy.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TestWalletService.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TestWalletService.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TransactionApi.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TransactionApi.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TransactionApiHelpers.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TransactionApiHelpers.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/TransactionPool.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/TransactionPool.cpp.o"
+  "/home/ubuntu/dronecoin/tests/UnitTests/main.cpp" "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/UnitTests.dir/UnitTests/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  "../../external/gtest/include"
+  "../../external/gtest"
+  "../../tests/../version"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ubuntu/dronecoin/build/release/external/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/PaymentGate.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Wallet.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/tests/CMakeFiles/TestGenerator.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/InProcessNode.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/NodeRpcProxy.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Rpc.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Http.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Transfers.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Serialization.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/System.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Logging.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/BlockchainExplorer.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Common.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/CryptoNoteCore.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/src/CMakeFiles/Crypto.dir/DependInfo.cmake"
+  "/home/ubuntu/dronecoin/build/release/external/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
